97d8eebd7f63dddbd974111d89e7c549cc7cba12
kellycesario <kellycesario@gmail.com>
2023-07-26 23:50:36 -0300

    Add: create the modal and logic for a PWA implementation

---
+++ src/components/molecules/Modal/index.vue
@@ -0,0 +1,87 @@
+<script setup>
+import Button from "@atoms/Button/index.vue"
+import { onMounted, ref } from "vue"
+
+const showModal = ref(true)
+let deferredPrompt = null
+
+const closeModal = () => {
+  showModal.value = false
+}
+
+const installApp = () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt()
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt")
+      } else {
+        console.log("User dismissed the install prompt")
+      }
+      deferredPrompt = null
+      closeModal()
+    })
+  }
+}
+
+onMounted(() => {
+  if (
+    !window.matchMedia("(display-mode: standalone)").matches &&
+    !window.navigator.standalone
+  ) {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault()
+      deferredPrompt = e
+      showModal.value = true
+    })
+  }
+})
+</script>
+
+<template>
+  <article v-if="showModal" class="modal">
+    <div class="modal__content">
+      <p>Install the app to get the best experience!</p>
+      <div class="modal__buttons">
+        <Button isButton @click="installApp" label="Install App" />
+        <Button isButton @click="closeModal" label="Close" />
+      </div>
+    </div>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.modal {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100%;
+  background-color: var(--darkGray);
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    margin: 0 auto;
+    padding: 0.2rem;
+    background-color: var(--offWhite);
+    border: 2px solid var(--green);
+    text-align: center;
+    width: 80%;
+    height: 50%;
+    @include default-text;
+  }
+
+  &__buttons {
+    display: flex;
+    gap: 2rem;
+  }
+}
+</style>
--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -3,9 +3,12 @@ import Hero from "@molecules/Hero/index.vue";
 import CardsWrapper from "@organisms/CardsWrapper/index.vue";
 import Facilities from "@organisms/Facilities/index.vue";
 import Discover from "@molecules/DiscoverBanner/index.vue";
+import Modal from "@molecules/Modal/index.vue";
+
 </script>
 
 <template>
+  <Modal />
   <Hero />
   <CardsWrapper />
   <Facilities />
