ece381b2d336562992473799a4500026765ddc40
kellycesario <kellycesario@gmail.com>
2023-07-27 08:03:58 -0300

    Fix: remove modal from homepage

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -3,12 +3,10 @@ import Hero from "@molecules/Hero/index.vue";
 import CardsWrapper from "@organisms/CardsWrapper/index.vue";
 import Facilities from "@organisms/Facilities/index.vue";
 import Discover from "@molecules/DiscoverBanner/index.vue";
-import Modal from "@molecules/Modal/index.vue";
 
 </script>
 
 <template>
-  <Modal />
   <Hero />
   <CardsWrapper />
   <Facilities />
