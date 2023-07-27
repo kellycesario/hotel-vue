ff93bc8a586953394a7aab7d7fbe4730503d08de
kellycesario <kellycesario@gmail.com>
2023-07-27 16:38:03 -0300

    update: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,41 @@
-const path = require("path");
-const { mergeConfig, loadConfigFromFile } = require("vite");
-
-module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+import path from 'path'
+import { loadConfigFromFile, mergeConfig } from 'vite'
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-    "@storybook/addon-postcss",
-    "@storybook/preset-scss",
-    "storybook-dark-mode",
+    'storybook-dark-mode',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          implementation: require('sass'),
+        },
+      },
+    },
   ],
-  framework: "@storybook/vue3",
-  core: {
-    builder: "@storybook/builder-vite",
+
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
   },
   features: {
     storyStoreV7: true,
   },
-  staticDirs: ["../public"],
+  staticDirs: ['../public'],
 
   async viteFinal(config, { configType }) {
     const { config: userConfig } = await loadConfigFromFile(
-      path.resolve(__dirname, "../vite.config.js")
-    );
+      path.resolve(__dirname, '../vite.config.js')
+    )
 
     return mergeConfig(config, {
       css: {
@@ -38,19 +49,21 @@ module.exports = {
       plugins: [],
       resolve: {
         alias: {
-          "@": path.resolve(__dirname, "../src"),
-          "@atoms": path.resolve(__dirname, "../src/components/atoms"),
-          "@ions": path.resolve(__dirname, "../src/components/ions"),
-          "@molecules": path.resolve(__dirname, "../src/components/molecules"),
-          "@organisms": path.resolve(__dirname, "../src/components/organisms"),
-          "@images": path.resolve(__dirname, "../src/assets/images"),
-          "@styles": path.resolve(__dirname, "../src/assets/styles"),
-          "@db": path.resolve(__dirname, "../src/db"),
-          "@views": path.resolve(__dirname, "../src/views"),
-          "@router": path.resolve(__dirname, "../src/router"),
-          "@store": path.resolve(__dirname, "../src/store"),
+          '@': path.resolve(__dirname, '../src'),
+          '@atoms': path.resolve(__dirname, '../src/components/atoms'),
+          '@ions': path.resolve(__dirname, '../src/components/ions'),
+          '@molecules': path.resolve(__dirname, '../src/components/molecules'),
+          '@organisms': path.resolve(__dirname, '../src/components/organisms'),
+          '@images': path.resolve(__dirname, '../src/assets/images'),
+          '@styles': path.resolve(__dirname, '../src/assets/styles'),
+          '@db': path.resolve(__dirname, '../src/db'),
+          '@views': path.resolve(__dirname, '../src/views'),
+          '@router': path.resolve(__dirname, '../src/router'),
+          '@store': path.resolve(__dirname, '../src/store'),
         },
       },
-    });
+    })
   },
-};
+}
+
+export default config
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '../src/assets/styles/_globals.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
