fc61508b1d772cb6d8115a18574d2323103dd897
kellycesario <kellycesario@gmail.com>
2023-08-14 11:18:35 -0300

    feat: install hotjar

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
-import { createVuestic } from "vuestic-ui";
+import Hotjar from '@hotjar/browser'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { createVuestic } from 'vuestic-ui'
+import App from './App.vue'
+import router from './router'
 
-const pinia = createPinia();
+const pinia = createPinia()
+const app = createApp(App)
+const siteId = 3612906
+const hotjarVersion = 6
 
-const app = createApp(App);
-app.use(createVuestic());
-app.use(pinia);
-app.use(router);
-app.mount("#app");
+app.use(createVuestic())
+app.use(pinia)
+app.use(router)
+app.mount('#app')
+Hotjar.init(siteId, hotjarVersion)
